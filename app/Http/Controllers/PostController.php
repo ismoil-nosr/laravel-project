32786ace68abd7094ec16cf589c2fed446a345dc
Ismoil Nosr <ismoil.nosr@gmail.com>
2020-09-16 07:47:54 +0500

    make it RESTful applicable

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -12,27 +13,51 @@ class PostController extends Controller
         return view('index', compact('posts'));
     }
 
-    public function about(Post $post)
-    {
-        return view('posts.about', compact('post'));
-    }
-
     public function create()
     {
         return view('posts.create');
     }
 
-    public function store()
+    public function show(Post $post)
     {
-        $this->validate(request(), [
+        return view('posts.about', compact('post'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
             'slug' => 'required|unique:posts',
             'title' => 'required|max:100|min:5',
             'short_body' => 'required|max:255|min:10',
             'full_body' => 'required|min:10'
         ]);
 
-        $post = Post::create(request()->all());
-        return redirect('/');
+        Post::create($request->all());
+        return redirect('/posts/' . $request->input('slug'));
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $attributes = $request->validate([
+            'slug' => 'required|unique:posts',
+            'title' => 'required|max:100|min:5',
+            'short_body' => 'required|max:255|min:10',
+            'full_body' => 'required|min:10'
+        ]);
+
+        $post->update($attributes);
+        return redirect('/posts/' . $request->input('slug'));
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect('/posts');
     }
 
 }
